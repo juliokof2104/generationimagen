bbc6621c26cf1968dee3d2d34202b896a01ab7b0
juliokof2104 <126487919+juliokof2104@users.noreply.github.com>
2025-06-22 22:59:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+
 app.use(express.json());
 app.use(cors({
   origin: "https://generationimagen.onrender.com"
